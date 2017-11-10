db78029e09fa8d49e9a5a55348a67914e18d19d2
Dylan Goss <Dylangoss21@yahoo.com>
2017-11-10 16:24:04 -0700

    fixed gramar in about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,12 +9,12 @@ class About extends Component {
                     <div className='aboutMeText'>
                         <div className='aboutMeTitleContainer'>
                         <p  id='About' className='aboutMeTitle'>About Me</p></div>
-                        <p>Through out High School I was always a tech savvy kind of person and enjoyed working 
+                        <p>Throughout High School I was always a tech savvy kind of person and enjoyed working 
                         with computers. Originally, I was going to Chippewa Valley Technical College for their
                         IT Networking Specialist program, but, ended up wanting to change my major over to Web 
-                        Development. That is then when I discovered about Coding Boot camps and came across 
-                        Dev Mountain, where I am currently a student in Phoenix, AZ. At Dev Mountain I was able 
-                        to quickly learn HTML, CSS, React, JavaScript, SASS, and AngularJS. I will be graduating 
+                        Development. That is when I discovered Coding Boot camps and came across 
+                        DevMountain, where I am currently a student in Phoenix, AZ. At DevMountain I was able 
+                        to quickly learn HTML, CSS, React, JavaScript, SASS, NodeJS, and AngularJS. I will be graduating 
                         on November 17th and then going back to Chippewa Falls to hopefully find a job in either
                         Front-End, Back-End or Full Stack Development.</p>
                 </div>
