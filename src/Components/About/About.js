871e06582bf4db8811c9c869747784e997ebe15b
Dylan Goss <Dylangoss21@yahoo.com>
2017-11-15 20:33:52 -0700

    added group project

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './About.css';
+import Dylan from '../../Images/IMG_2079 (2).jpg';
 
 class About extends Component {
     render() {
@@ -9,7 +10,9 @@ class About extends Component {
                     <div className='aboutMeText'>
                         <div className='aboutMeTitleContainer'>
                         <p  id='About' className='aboutMeTitle'>About Me</p></div>
-                        <p>Throughout High School I was always a tech savvy kind of person and enjoyed working 
+                        <div className='aboutMeContainer'>
+                        <img className='pictureOfMe' src={Dylan}/>
+                        <p className='aboutMeText'>Throughout High School I was always a tech savvy kind of person and enjoyed working 
                         with computers. Originally, I was going to Chippewa Valley Technical College for their
                         IT Networking Specialist program, but, ended up wanting to change my major over to Web 
                         Development. That is when I discovered Coding Boot camps and came across 
@@ -17,6 +20,7 @@ class About extends Component {
                         to quickly learn HTML, CSS, React, JavaScript, SASS, NodeJS, and AngularJS. I will be graduating 
                         on November 17th and then going back to Chippewa Falls to hopefully find a job in either
                         Front-End, Back-End or Full Stack Development.</p>
+                        </div>
                 </div>
                 </div>
             </div>
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -14,12 +14,19 @@ class Portfolio extends Component {
 
                     <div className='projectsContainer'>
                         <div className='project'>
-                            <div><p className='tempText'>Coming Soon!</p></div>
+                        <a href='http://waypointhomes.surge.sh/#/'><div className='project1Image'></div></a>
+                            <div className='repoLogoContainer'>
+                            <a href='https://github.com/simoneholder1/WaypointHomes/tree/master/waypoint/Waypoint-Homes'>
                             <img className='repoLogo' src={GithubLogoBlack} />
+                            </a>
+                            </div>
                         </div>
                         <div className='project'>
-                            <div><p className='tempText'>Coming Soon!</p></div>
-                            <img className='repoLogo' src={GithubLogoBlack} />
+                        <div className='project2Image'></div>
+                        <div className='tempText'>Coming Soon!</div>
+                        <div className='repoLogoContainer'>
+                            {/* <img className='repoLogo' src={GithubLogoBlack} /> */}
+                        </div>
                         </div>
                     </div>
                 </div>
