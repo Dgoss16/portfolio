584f59cf198460dbf5c5a4aae6bd6f9ad59f490e
Dylan <dylangoss21@yahoo.com>
2018-06-24 15:56:21 -0500

    changes to about me

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -12,12 +12,13 @@ class About extends Component {
                         <p  id='About' className='aboutMeTitle'>About Me</p></div>
                         <div className='aboutMeContainer'>
                         <img className='pictureOfMe' src={Dylan}/>
-                        <p>Throughout High School I was always a tech savvy kind of person and enjoyed working 
-                        with computers. Originally, I was going to Chippewa Valley Technical College for their
-                        IT Networking Specialist program, but, ended up finding an interest in Web 
-                        Development instead. That is when I discovered Coding Boot Camps and came across 
-                        DevMountain. At DevMountain I was able to quickly learn HTML, CSS, React, JavaScript, SASS, and NodeJS. I graduated 
-                        on November 17th and am currently searching for a job as a Junior Web Developer.</p>
+                        <div className='aboutMeText'>
+                            <p>Throughout High School, I grew very fond of technology and enjoyed working 
+                            with computers. That eventually developed into the love and passion that I now have for
+                            coding. Currently, I have two projects built using ReactJs, JavaScript, NodeJs and PostgresSql.
+                            Now I am focusing on developing my skills with jQuery and on the side am developing a web
+                            application using React.</p>
+                        </div>
                         </div>
                 </div>
                 </div>
