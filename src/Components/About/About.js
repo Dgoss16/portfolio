52cd3be295650965e9d89aecc2e0157ba9797b6c
Dylan Goss <dylangoss21@gmail.com>
2020-04-14 10:26:20 -0500

    re did about section completely

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,23 +5,22 @@ import Dylan from '../../Images/IMG_2079 (2).jpg';
 class About extends Component {
     render() {
         return (
-            <div className='about'>
-                <div className='aboutContainer'>
-                    <div className='aboutMeTextContainer'>
-                        <div className='aboutMeTitleContainer'>
-                        <p  id='About' className='aboutMeTitle'>About Me</p></div>
-                        <div className='aboutMeContainer'>
-                        <img className='pictureOfMe' src={Dylan}/>
-                        <div className='aboutMeText'>
-                            <p>Throughout High School, I grew very fond of technology and enjoyed working 
-                            with computers. That eventually developed into the love and passion that I now have for
-                            coding. Currently, I have two projects built using ReactJs, JavaScript, NodeJs and PostgresSql.
-                            Now I am focusing on developing my skills with Java and on the side am developing a web
-                            application using React.</p>
-                        </div>
-                        </div>
+            <div className='aboutComponent'>
+                <div className='aboutMeTitleContainer'>
+                    <p id='About' className='aboutMeTitle'>About Me</p>
+                </div>
+                <div className='aboutMeTextPicContainer'>
+                <div className='aboutMePicContainer'>
+                    <img className='aboutMePic' src={Dylan}/>
+                </div>
+                <div className='aboutMeTextContainer'>
+                    <p className='aboutMeText'>Throughout High School, I grew very fond of technology and enjoyed working with 
+                    computers. That eventually developed into the love and passion that I now have for coding. Currently, I have 
+                    two projects built using ReactJs, JavaScript, NodeJs and PostgresSql. Now I am focusing on developing my skills 
+                    with Java and on the side am developing a web application using React.</p>
                 </div>
                 </div>
+
             </div>
         )
     }
